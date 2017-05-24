11ef000802502214a208cd00a71e41c5c7fd8e2b
munhoven <munhoven@gmail.com>
2017-05-25 01:00:35 +0200

    Added dummy image file (NZ foxglove)
    placeholders for actions and outlets linked.

---
+++ SpiceCollector/SpiceViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SpiceViewController.swift
+//  SpiceCollector
+//
+//  Created by Tom Munhoven on 25/05/2017.
+//  Copyright © 2017 Tom Munhoven. All rights reserved.
+//
+
+import UIKit
+
+class SpiceViewController: UIViewController {
+
+    @IBOutlet weak var spiceImage: UIImageView!
+    
+    @IBOutlet weak var spiceTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    
+    
+    
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    
+    
+    
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    
+    
+        
+    }
+    
+    
+}
